4f1714e7acd4f6a640949b3a4141bfd1fa254685
Rafler <amuronyk@gmail.com>
2020-08-06 12:53:55 +0300

    updates

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import cv from '../../data/cv.pdf';
 import './Contact.scss'
 
 const Contact = ({content}) => {
@@ -7,7 +8,10 @@ const Contact = ({content}) => {
         <section className="contact" id="contact">
             <h2 className="title">{content.TITLE}</h2>
             <p className="description">{content.DESCRIPTION}</p>
-            <a className="contact__button" href="mailto:amuronyk@gmail.com">{content.BUTTON}</a>
+            <div className="contact__buttons">
+                <button className="contact__button"><a className="contact__button__text" href="mailto:amuronyk@gmail.com">{content.BUTTON.MAIL}</a></button>
+                <button className="contact__button reversed"><a className="contact__button__text " href={cv} download="CV Andrii Myroniuk">{content.BUTTON.CV}</a></button>
+            </div>
             <div className="contact__media">
                 <a target="_blank" href="https://www.instagram.com/andrii.lv/" className="contact__media__link inst"/>
                 <a target="_blank" href="https://www.linkedin.com/in/andrii-myroniuk-573921155/"
